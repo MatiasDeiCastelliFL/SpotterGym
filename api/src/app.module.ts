b50530e9d9921226bed128ece4bc03cd35bd2882
emanuelgauler <emanuelgauler@gmail.com>
2024-03-13 11:29:08 -0300

    se agregó el módulo para instructores

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { InstructorsModule } from './instructors/instructors.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
-const feacture = [UserModule];
+const feacture = [UserModule, InstructorsModule];
 @Module({
   imports: [MongooseModule.forRoot(process.env.CONEXION_DB), ...feacture],
 })
