c6c35d870f4981dd83b62cb435e448ed9ca7aa79
alcaponesss <46201660+alcaponesss@users.noreply.github.com>
2024-03-12 22:41:09 -0300

    updating config nest

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const feacture = [UserModule];
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(process.env.CONEXION_DB), ...feacture],
 })
 export class AppModule {}
