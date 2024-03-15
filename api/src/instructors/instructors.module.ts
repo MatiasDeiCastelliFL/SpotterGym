912fe9aa6cbf4e64b53bbb170b71f9b83bf8c6d6
emanuelgauler <emanuelgauler@gmail.com>
2024-03-15 19:35:36 -0300

    se modificó para usar la interfaz de base de datos

--- api/src/instructors/instructors.module.ts
+++ api/src/instructors/instructors.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { InstructorsController } from './instructors.controller';
 import { InstructorsService } from './instructors.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { Instructor } from './instructor.entity';
+import { InstructorSchema } from './instructor.schema'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature({
+      name: Instructor.name,
+      schema: InstructorSchema,
+    }),
+  ],
   controllers: [InstructorsController],
-  providers: [InstructorsService],
+  providers: [
+    InstructorsService,
+    { provide: InstructorRepository, useClass: MongooInstructorRepository },
+  ],
 })
 export class InstructorsModule {}
