e8fcd9fa21c5386409a19704a083df3648b4d427
emanuelgauler <emanuelgauler@gmail.com>
2024-03-15 19:36:39 -0300

    se agregó el esquema para instructores

---
+++ api/src/instructors/instructor.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Schema()
+class Instructor {
+  @Prop({ required: true })
+  firstName: string;
+  @Prop({ required: true })
+  lastName: string;
+  @Prop()
+  phone: string;
+  @Prop()
+  description: string;
+  @Prop({ unique: true })
+  email: string;
+  @Prop()
+  password: string;
+}
+
+export type InstructorDocument = Instructor & Document;
+export const InstructorModel = SchemaFactory.createForClass(Instructor);
+export type InstructorModel = Model<InstructorDocument>;
