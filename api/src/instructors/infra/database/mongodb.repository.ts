bb461e6cd39fd6c91877ff854903967f8a9dc3f6
emanuelgauler <emanuelgauler@gmail.com>
2024-03-15 23:50:37 -0300

    primera implementación de los métodos del repositorio

--- api/src/instructors/infra/database/mongodb.repository.ts
+++ api/src/instructors/infra/database/mongodb.repository.ts
@@ -1,13 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateInstructorDTO } from 'src/instructors/dto/create';
 import { Instructor } from 'src/instructors/instructor.entity';
 import { InstructorRepository } from 'src/instructors/instructor.repository';
+import { InstructorModel } from 'src/instructors/instructor.schema';
 
+@Injectable()
 export class MongoInstructorRepository implements InstructorRepository {
+  constructor(
+    @InjectModel(Instructor.name) private instructorsModel: InstructorModel,
+  ) {}
   findAll(): Promise<Instructor[]> {
-    throw new Error('Method not implemented.');
+    return this.instructorsModel.find();
   }
-  create(createInstructorDTO: CreateInstructorDTO) {
-    createInstructorDTO;
-    throw new Error('Method not implemented.');
+  create(instructor: CreateInstructorDTO) {
+    this.instructorsModel.create(instructor);
   }
 }
