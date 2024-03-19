2823c12f261baf510200c0427e2a9b9902850175
emanuelgauler <emanuelgauler@gmail.com>
2024-03-18 23:40:27 -0300

    se quitó el log al puerto

--- api/src/utils/common.ts
+++ api/src/utils/common.ts
@@ -4,7 +4,6 @@ dotenv.config();
 const PROTOCOL = process.env.PROTOCOL;
 const DOMAIN = process.env.DOMAIN;
 const inNotHTTPS = process.env.PORT !== '443';
-console.log('>> UTILS PORT', process.env.PORT);
 const PORT = inNotHTTPS ? ':'.concat(process.env.PORT) : '';
 
 export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${PORT}`;
