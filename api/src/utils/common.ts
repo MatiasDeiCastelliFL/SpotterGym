1dab43d3731721fb279e51c68f563ed866e180bf
emanuelgauler <emanuelgauler@gmail.com>
2024-03-18 23:41:05 -0300

    TYPO: un error de tipeo

--- api/src/utils/common.ts
+++ api/src/utils/common.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 const PROTOCOL = process.env.PROTOCOL;
 const DOMAIN = process.env.DOMAIN;
-const inNotHTTPS = process.env.PORT !== '443';
-const PORT = inNotHTTPS ? ':'.concat(process.env.PORT) : '';
+const isNotHTTPS = process.env.PORT !== '443';
+const PORT = isNotHTTPS ? ':'.concat(process.env.PORT) : '';
 
 export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${PORT}`;
