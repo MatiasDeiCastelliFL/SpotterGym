0876706eaa8f046d87210665ce336c6cb68036bd
emanuelgauler <emanuelgauler@gmail.com>
2024-03-19 19:18:33 -0300

    se agregó el puerto a exportar

--- api/src/utils/common.ts
+++ api/src/utils/common.ts
@@ -4,6 +4,7 @@ dotenv.config();
 const PROTOCOL = process.env.PROTOCOL;
 const DOMAIN = process.env.DOMAIN;
 const isNotHTTPS = process.env.PORT !== '443';
-const PORT = isNotHTTPS ? ':'.concat(process.env.PORT) : '';
+const URL_PORT = isNotHTTPS ? ':'.concat(process.env.PORT) : '';
 
-export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${PORT}`;
+export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${URL_PORT}`;
+export const PORT = process.env.PORT;
