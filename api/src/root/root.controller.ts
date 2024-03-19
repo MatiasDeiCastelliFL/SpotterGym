fef94787da6a2910e171b18b667ddf2618ec12ce
emanuelgauler <emanuelgauler@gmail.com>
2024-03-18 23:11:42 -0300

    se agregó una URL base

--- api/src/root/root.controller.ts
+++ api/src/root/root.controller.ts
@@ -1,9 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { SPOTTER_GYM_URL } from '../utils/common';
 
 @Controller()
 export class RootController {
   @Get()
   index() {
-    return 'Index of Rest API for Spotter Gym';
+    return {
+      message: 'Welcome to the index of Rest API for Spotter Gym',
+      links: {
+        instructors: {
+          index: `${SPOTTER_GYM_URL}/instructors`,
+        },
+        roles: {
+          index: `${SPOTTER_GYM_URL}/roles`,
+        },
+      },
+    };
   }
 }
