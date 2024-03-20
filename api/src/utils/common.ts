66138898e8585d80eb2d323364e1420374ef9b6f
emanuelgauler <emanuelgauler@gmail.com>
2024-03-19 22:32:29 -0300

    se cambió la constante del puerto

--- api/src/utils/common.ts
+++ api/src/utils/common.ts
@@ -4,6 +4,7 @@ dotenv.config();
 const PROTOCOL = process.env.PROTOCOL;
 const DOMAIN = process.env.DOMAIN;
 const inNotHTTPS = process.env.PORT !== '443';
-const PORT = inNotHTTPS ? ':'.concat(process.env.PORT) : '';
+const URL_PORT = inNotHTTPS ? ':'.concat(process.env.PORT) : '';
 
-export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${PORT}`;
+export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${URL_PORT}`;
+export const PORT = process.env.PORT;
