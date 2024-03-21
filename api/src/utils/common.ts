68baeebc181cccccfe164e059e3700af93a7165c
Emanuel Gauler <emanuelgauler@gmail.com>
2024-03-21 12:13:28 -0300

    Update common.ts
    Se cambió el tipo de dato del puerto

--- api/src/utils/common.ts
+++ api/src/utils/common.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const PROTOCOL = process.env.PROTOCOL;
 const DOMAIN = process.env.DOMAIN;
 
-const isNotHTTPS = process.env.PORT !== 443;
+const isNotHTTPS = process.env.PORT !== '443';
 const URL_PORT = isNotHTTPS ? ':'.concat(process.env.PORT) : '';
 
 export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${URL_PORT}`;
