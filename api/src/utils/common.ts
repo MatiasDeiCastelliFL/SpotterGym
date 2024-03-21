139c28a7c7197f9a40a92192864c80468bd258a9
Emanuel Gauler <emanuelgauler@gmail.com>
2024-03-21 12:11:35 -0300

    Update common.ts
    Eliminado de comentarios

--- api/src/utils/common.ts
+++ api/src/utils/common.ts
@@ -3,13 +3,9 @@ dotenv.config();
 
 const PROTOCOL = process.env.PROTOCOL;
 const DOMAIN = process.env.DOMAIN;
-<<<<<<< HEAD
-const inNotHTTPS = process.env.PORT !== '443';
-const URL_PORT = inNotHTTPS ? ':'.concat(process.env.PORT) : '';
-=======
-const isNotHTTPS = process.env.PORT !== '443';
+
+const isNotHTTPS = process.env.PORT !== 443;
 const URL_PORT = isNotHTTPS ? ':'.concat(process.env.PORT) : '';
->>>>>>> origin/develop
 
 export const SPOTTER_GYM_URL = `${PROTOCOL}://${DOMAIN}${URL_PORT}`;
 export const PORT = process.env.PORT;
