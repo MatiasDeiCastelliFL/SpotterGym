b8e8120824dc75b03b06e68eccacc4d9ad045507
emanuelgauler <emanuelgauler@gmail.com>
2024-03-21 21:46:01 -0300

    se creó el módulo para usuarios

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RolModule } from './rol/rol.module';
 import { InstructorsModule } from './instructors/instructors.module';
 import { RootController } from './root/root.controller';
+import { UsersModule } from './users/users.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const features = [];
 features.push(InstructorsModule);
 features.push(RolModule);
+features.push(UsersModule);
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.CONEXION_DB), ...features],
