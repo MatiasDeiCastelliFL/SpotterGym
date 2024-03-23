217d753d4d216300d56846c310f86aa855ba264b
Emanuel Gauler <emanuelgauler@gmail.com>
2024-03-23 11:48:06 -0300

    se agregó la etiqueta de 'Ususario'

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,31 +5,35 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PORT } from './utils/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  //const app = await NestFactory.create(AppModule, { bodyParser: false });
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+   const app = await NestFactory.create(AppModule);
+   //const app = await NestFactory.create(AppModule, { bodyParser: false });
+   app.useGlobalPipes(new ValidationPipe());
+   app.useGlobalPipes(
+      new ValidationPipe({
+         whitelist: true,
+         forbidNonWhitelisted: true,
+         transform: true,
+      }),
+   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Spotter Gym Rest API Documentation')
-    .setDescription('Documentación de la REST Api de Spotter Gym')
-    .addServer(`http://localhost:${PORT}/`, 'Local Server')
-    .addServer('https://spottter-gym.onrender.com/', 'Develop Server')
-    .setVersion('1.0')
-    .addTag('index', 'Operaciones sobre la raíz')
-    .addTag('Instructores', 'Operaciones sobre los instructores')
-    .addTag('Roles', 'Operaciones sobre los roles definidos en el gimnasio')
-    .build();
+   const config = new DocumentBuilder()
+      .setTitle('Spotter Gym Rest API Documentation')
+      .setDescription('Documentación de la REST Api de Spotter Gym')
+      .addServer(`http://localhost:${PORT}/`, 'Local Server')
+      .addServer('https://spottter-gym.onrender.com/', 'Develop Server')
+      .setVersion('1.0')
+      .addTag('index', 'Operaciones sobre la raíz')
+      .addTag('Instructores', 'Operaciones sobre los instructores')
+      .addTag('Roles', 'Operaciones sobre los roles definidos en el gimnasio')
+      .addTag(
+         'Usuarios',
+         'Operaciones para el control y acceso de los usuarios',
+      )
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documentation', app, document);
-  app.enableCors();
-  await app.listen(PORT || 3000);
+   const document = SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup('documentation', app, document);
+   app.enableCors();
+   await app.listen(PORT || 3000);
 }
 bootstrap();
