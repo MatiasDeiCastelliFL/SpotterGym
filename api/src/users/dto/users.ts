d01e970e783a1b39030a670d45160180e0a717ec
Emanuel Gauler <emanuelgauler@gmail.com>
2024-03-23 12:25:47 -0300

    se agregó la descripción de las respuestas y cuerpo de las solicitudes

--- api/src/users/dto/users.ts
+++ api/src/users/dto/users.ts
@@ -1,28 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class UserBody {
-  @IsNotEmpty({ message: 'email must be present' })
-  @IsEmail()
-  email: string;
+   @ApiProperty({
+      description: 'Correo electrónico del usuario',
+      example: 'emanuel-gauler@spotter-gym.com.ar',
+   })
+   @IsNotEmpty({ message: 'email must be present' })
+   @IsEmail()
+   email: string;
 
-  @IsNotEmpty({ message: 'password must be present' })
-  password: string;
+   @ApiProperty({
+      description: 'Contraseña del usuario',
+      example: 'twreqt2537',
+   })
+   @IsNotEmpty({ message: 'password must be present' })
+   password: string;
 }
 
 export class UserResponse {
-  id_: string;
-  email: string;
-  role: string;
+   id_: string;
+   email: string;
+   role: string;
 }
 
 export class UsersResponse {
-  message: string;
-  users: UserResponse[];
+   message: string;
+   users: UserResponse[];
 
-  static NoUsers(): UsersResponse {
-    return {
-      message: 'No users found',
-      users: [],
-    };
-  }
+   static NoUsers(): UsersResponse {
+      return {
+         message: 'No users found',
+         users: [],
+      };
+   }
 }
--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,38 +1,128 @@
 import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Post,
+   BadRequestException,
+   Body,
+   Controller,
+   Get,
+   Post,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserBody as UserBodyDTO, UsersResponse } from './dto/users';
 import { JWT_SECRET } from 'src/utils/common';
 import { sign } from 'jsonwebtoken';
+import {
+   ApiBadRequestResponse,
+   ApiCreatedResponse,
+   ApiForbiddenResponse,
+   ApiOkResponse,
+   ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Usuarios')
 @Controller('users')
 export class UsersController {
-  constructor(private service: UsersService) {}
-  @Get()
-  index(): UsersResponse {
-    return UsersResponse.NoUsers();
-  }
+   constructor(private service: UsersService) {}
 
-  @Post('sign-in')
-  async login(@Body() data: UserBodyDTO) {
-    try {
-      const result = await this.service.sign_in_with(data);
-      const token = sign(result, JWT_SECRET, {
-        algorithm: 'HS256',
-      });
-      console.warn('>> USER', data.email, 'GRANTED');
-      return {
-        message: 'Acceso garantisado',
-        token,
-      };
-    } catch (error) {
-      console.error('>> SIGN IN ERROR:', error);
-      throw new BadRequestException(error);
-    }
-  }
+   @ApiOkResponse({
+      description:
+         'Retorna una lista de los usuarios registrados en el sistema',
+      content: {
+         'application/json': {
+            schema: {
+               type: 'object',
+               properties: {
+                  message: {
+                     type: 'string',
+                     example: 'Usuarios registrados',
+                  },
+                  users: {
+                     type: 'array',
+                     items: {
+                        type: 'object',
+                        properties: {
+                           email: {
+                              type: 'string',
+                              example: 'emanuel-gauler@spotter-gym.com.ar',
+                           },
+                           user_id: {
+                              type: 'string',
+                              example: '65fe2a8667bb7d6b0afe191e',
+                           },
+                           role_name: { type: 'string', example: 'cliente' },
+                        },
+                     },
+                  },
+               },
+            },
+         },
+      },
+   })
+   @Get()
+   index(): UsersResponse {
+      return UsersResponse.NoUsers();
+   }
+
+   @ApiCreatedResponse({
+      description: 'Retorna un mensaje y el token de acceso al sistema',
+      content: {
+         'application/json': {
+            schema: {
+               type: 'object',
+               properties: {
+                  message: { type: 'string', example: 'Acceso garantizado' },
+                  token: {
+                     type: 'string',
+                     example:
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjVmZTJhODY2N2JiN2Q2YjBhZmUxOTFlIiwicm9sZV9uYW1lIjoiY2xpZW50ZSIsImlhdCI6MTUxNjIzOTAyMn0.442YBdadlaBf-1YO23LsCYxH0UQtAeUQ-UFb5CDOxS0',
+                  },
+               },
+            },
+         },
+      },
+   })
+   @ApiBadRequestResponse({
+      description: 'Cuando falta algún dato en el mensaje de la solicitud',
+      content: {
+         'application/json': {
+            schema: {
+               type: 'object',
+               properties: {
+                  message: {
+                     type: 'string',
+                     example: 'faltan datos para verificar el usuario',
+                  },
+               },
+            },
+         },
+      },
+   })
+   @ApiForbiddenResponse({
+      description: 'Cuando las credenciales no son válidas',
+      content: {
+         'application/json': {
+            schema: {
+               type: 'object',
+               properties: {
+                  message: { type: 'string', example: 'Acceso no autorizado' },
+               },
+            },
+         },
+      },
+   })
+   @Post('sign-in')
+   async login(@Body() data: UserBodyDTO) {
+      try {
+         const result = await this.service.sign_in_with(data);
+         const token = sign(result, JWT_SECRET, {
+            algorithm: 'HS256',
+         });
+         console.warn('>> USER', data.email, 'GRANTED');
+         return {
+            message: 'Acceso garantisado',
+            token,
+         };
+      } catch (error) {
+         console.error('>> SIGN IN ERROR:', error);
+         throw new BadRequestException(error);
+      }
+   }
 }
